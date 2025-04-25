39591d5174745d566f14f2c66463f979946d1c98
OC1024 <testudo_gruppe@yahoo.de>
2025-04-25 21:05:28 +0200

    fixing errors in data-updates, adding new_entries

--- data-updates.lua
+++ data-updates.lua
@@ -2,20 +2,33 @@ if mods["aai-industry"] then
   require("prototypes.compatibility.aai-industry")
 end
 
+
+alternative_recipes = alternative_recipes or {}
+
+local new_entries = {
+    ["stone-brick"] = {"lava-to-stone-brick","stone-brick",}, -- add new alt recipe, which is more important
+    ["concrete"] = {"casting-concrete-recipe","concrete-from-molten-iron","concrete",}, -- overwrite with new order
+    ["refined-concrete"] = {"cating-concrete","casting-refined-concrete-recipe","refined-concrete",},
+}
+for i, r in pairs(new_entries) do
+    alternative_recipes[i] = r
+end
+
 if casting_materials then
     casting_materials["stone"] = {
-        liquid = "lava",
+        fluid = "lava",
         ratio = 10,
         energy_required = 0.8
     }
     if data.raw["item"]["sand"] then
     casting_materials["sand"] = {
-        liquid = "lava",
+        fluid = "lava",
         ratio = 5,
         energy_required = 0.4
     }
     end
 end
+
 require("__OCs_base_assets__.prototypes.utils.casting-generator")
 
 require("prototypes.recipe-updates")
